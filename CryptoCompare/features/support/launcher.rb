12b3f57e68545ba55730f85cbe71b9f24abb786a
Unknown <hvaliji@cryptocompare.com>
2018-04-19 17:19:16 +0100

    Update launcher

--- CryptoCompare/features/support/launcher.rb
+++ CryptoCompare/features/support/launcher.rb
@@ -1,6 +1,7 @@
 require 'selenium-webdriver'
 require 'watir'
 require 'watir-webdriver'
+require 'rspec/retry'
 
 def driver
 	@driver ||= Selenium::WebDriver.for :chrome
@@ -10,20 +11,20 @@ def wait
 	@wait ||= Selenium::WebDriver::Wait.new(:timeout => 10)
 end
 
-
-attempts = 0
 Before('@global') do 
 	Selenium::WebDriver::Chrome.driver_path="chromedriver"
 	driver.navigate.to "https://rc-server.cryptocompare.com:3750"
 	driver.manage.window.resize_to(1280,720)
-	rescue Net::ReadTimeout => 
-    if attempts == 0
-      attempts += 1
-      retry
-    else
-      raise
-    end
-  end
+	driver.manage.timeouts.page_load = 300
+end
+
+RSpec.configure do |config|
+  # show retry status in spec process
+  config.verbose_retry = true
+  # Try five times (retry 4 times)
+  config.default_retry_count = 5
+  # Only retry when Selenium raises Net::ReadTimeout
+  config.exceptions_to_retry = [Net::ReadTimeout]
 end
 
 
