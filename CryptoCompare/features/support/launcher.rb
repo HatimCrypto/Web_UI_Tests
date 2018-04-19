80568674a816df92c62fc990be0b6b3761517d97
Unknown <hvaliji@cryptocompare.com>
2018-04-19 17:14:11 +0100

    Update launcher

--- CryptoCompare/features/support/launcher.rb
+++ CryptoCompare/features/support/launcher.rb
@@ -16,7 +16,7 @@ Before('@global') do
 	Selenium::WebDriver::Chrome.driver_path="chromedriver"
 	driver.navigate.to "https://rc-server.cryptocompare.com:3750"
 	driver.manage.window.resize_to(1280,720)
-	rescue Net::ReadTimeout => e
+	rescue Net::ReadTimeout => 
     if attempts == 0
       attempts += 1
       retry
